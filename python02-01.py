41c57c4f043e7afb739d2e4859a0bacd85ad6a22
oneq5637 <oneq5637@gmail.com>
2019-02-22 15:44:23 +0900

    파이썬 숫자형 사용하기

--- python02-01.py
+++ python02-01.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 주석을 남기기 위한 기호는
 # 파이썬이 지원하는 숫자형
 # 정수, 실수, 복소수, 8진수, 16진수
@@ -20,4 +21,36 @@ print("16진수 출력")
 print(0x1234)
 print(-0x1234)
 
+print("복소수 출력")
+a = 1 + 2j
+print(a)
+b = 3 - 4J
+print(b)
 
+a = 1+2j
+print("실수 부분 출력 : %s" % a.real)
+print("허수 부분 출력 : %s" % a.imag)
+print("켤레복소수 출력 : %s" % a.conjugate())
+print("절대갑 출력 : %s" % abs(a))
+
+# 숫자형을 활용하기 위한 연산자
+# 기본 4칙연산
+# +, -, *, /, **(제곱 연산), %(나머지 연산), //(나눈 몫만 출력)
+# 
+
+a = 3
+b = 4
+c = a + b
+print("a + b = %s" % c)
+c = a - b
+print("a - b = %s" % c)
+c = a * b
+print("a * b = %s" % c)
+c = a / b
+print("a / b = %s" % c)
+c = a % b
+print("a %s b = %s" % ("%",c))
+c = a ** b
+print("a ** b = %s" % c)
+c = a // b
+print("a // b = %s" % c)
